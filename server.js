b72c1ab0307e8bf90b21402651103a190e996131
Pham Huy <76714933+huyp26102000@users.noreply.github.com>
2025-02-08 17:43:16 +0700

    fix limit

--- server.js
+++ server.js
@@ -13,7 +13,10 @@ const UPLOAD_DIR = path.join(__dirname, "uploads");
 fs.ensureDirSync(UPLOAD_DIR);
 
 app.use(cors());
-app.use(express.json());
+// app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
+
 
 // ? Health Check
 app.get("/status", (req, res) => {
